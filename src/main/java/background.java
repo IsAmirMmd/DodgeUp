21d5fbb57557627e37fd8e04ea6d7b01152ce944
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-05-25 19:32:49 +0330

    background settings updated

--- src/main/java/background.java
+++ src/main/java/background.java
@@ -1,11 +1,27 @@
 import processing.core.PApplet;
+
 public class background {
-    public PApplet bgc = DodgeUp.pApplet;
-    public void setBackGround(){
+    public static PApplet bgc = DodgeUp.pApplet;
+    public DodgeUp dodge = new DodgeUp();
+
+    public static void setBackGround() {
         bgc.background(200);
 //        bottom line
         bgc.fill(150);
-        bgc.rect(0,531,400,600);
+        bgc.noStroke();
+        bgc.rect(200, 531, 400, 62);
+    }
+
+    public static void setScore() {
+        bgc.textSize(15);
+        bgc.fill(5);
+        bgc.text("score : " + bgc.frameCount / 10, 10, 20);
+    }
+
+    public static void setLife() {
+        bgc.textSize(15);
+        bgc.fill(150, 150, 250);
+        bgc.text("Life : " + 5 / 3, 350, 20);
     }
 
 }
