0a4a199a41a83ccf34f4473264335624f840d505
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-05-28 23:47:29 +0330

    reading from database and seting highScore

--- src/main/java/DodgeUp.java
+++ src/main/java/DodgeUp.java
@@ -1,12 +1,14 @@
 import processing.core.PApplet;
+
+
 import processing.sound.*;
-import processing.event.MouseEvent;
 import com.jsyn.util.soundfile.*;
 
 import java.util.ArrayList;
 
 public class DodgeUp extends PApplet {
     public static PApplet pApplet;
+
     private final int width = 400;
     private final int height = 600;
     public Human human = new Human();
@@ -14,15 +16,18 @@ public class DodgeUp extends PApplet {
     public int life = 15; // equal to 5
     public int score = 0;
     public boolean gameOver = false;
+    public static boolean HighScore = false;
 
-    public SoundFile soundFileGameOver;
-    public SoundFile startGame;
+    //  adding sound make a delay ...
+//  public SoundFile soundFileGameOver;
+//  public SoundFile startGame;
+
 
     public void setup() {
         pApplet = this;
-        soundFileGameOver = new SoundFile(this, "C:\\Users\\amir\\Desktop\\practice\\DodgeUp\\src\\test\\gameover.wav");
-        startGame = new SoundFile(this, "C:\\Users\\amir\\Desktop\\practice\\DodgeUp\\src\\test\\start.wav");
-        startGame.play();
+//        soundFileGameOver = new SoundFile(this, "C:\\Users\\amir\\Desktop\\practice\\DodgeUp\\src\\test\\gameover.wav");
+//        startGame = new SoundFile(this, "C:\\Users\\amir\\Desktop\\practice\\DodgeUp\\src\\test\\start.mp3");
+//        startGame.play();
         human.humanBody();
         block.makeBlocks();
     }
@@ -31,8 +36,6 @@ public class DodgeUp extends PApplet {
         if (!gameOver) {
             background.setBackGround();
             float humanC = human.getHeadXPos();
-            float humanL = human.getHeadXPos() - human.getHeadWidth() / 2;
-            float humanR = human.getHeadXPos() + human.getHeadWidth() / 2;
             float humanT = human.getHeadYPos() + human.getHeadHeight() / 2;
             float humanB = human.getHeadYPos() - human.getHeadHeight() / 2;
             human.humanFPS();
@@ -43,13 +46,12 @@ public class DodgeUp extends PApplet {
                 float blockR = b.getBlockX() + b.getWidth() / 2;
                 float blockT = b.getBlockY() + b.getHeight() / 2;
                 float blockB = b.getBlockY() - b.getHeight() / 2;
-//               if (humanL > blockL && humanR < blockR && humanT < blockT && humanB > blockB) {
                 if (humanC >= blockL && humanC <= blockR && humanT < blockT && humanB > blockB) {
                     life--;
                     if (life <= 1) {
-                        score = frameCount / 3;
+                        score = frameCount / 10;
                         gameOver = true;
-                        startGame.stop();
+//                        startGame.stop();
                         gameOver(score);
                     }
                 }
@@ -62,8 +64,6 @@ public class DodgeUp extends PApplet {
             textSize(15);
             fill(150, 150, 250);
             text("Life : " + life / 3, 350, 20);
-
-
         }
     }
 
@@ -79,17 +79,23 @@ public class DodgeUp extends PApplet {
 
     public void gameOver(int score) {
         background(0);
-        startGame.stop();
-        soundFileGameOver.cue(0);
-        soundFileGameOver.play();
+//        startGame.stop();
+//        soundFileGameOver.cue(0);
+//        soundFileGameOver.play();
+        SQL.addToSQL(score);
         textSize(25);
         text("your score is : " + score, 120, 200);
+        if (HighScore) {
+            fill(52, 143, 235);
+            text("you set a highScore!!!", 105, 295);
+        }
         fill(143, 11, 25);
         text("Exit", 150, 400);
     }
 
 
     public static void main(String[] args) {
+
         PApplet.main("DodgeUp", args);
     }
 }
